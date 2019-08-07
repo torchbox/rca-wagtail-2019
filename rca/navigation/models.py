a150f7455d66cd09e57f26d4aa04b082575612d5
kevinhowbrook <kbhowbrook@gmail.com>
2019-08-07 17:27:01 +0100

    Add new menu logic and audience links

--- rca/navigation/models.py
+++ rca/navigation/models.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ValidationError
+from django.forms.utils import ErrorList
 from modelcluster.models import ClusterableModel
 from wagtail.admin.edit_handlers import StreamFieldPanel
 from wagtail.contrib.settings.models import BaseSetting, register_setting
@@ -6,13 +8,29 @@ from wagtail.core.fields import StreamField
 
 
 class LinkBlock(blocks.StructBlock):
-    page = blocks.PageChooserBlock()
+    url = blocks.URLBlock(required=False)
+    page = blocks.PageChooserBlock(required=False)
     title = blocks.CharBlock(
-        help_text="Leave blank to use the page's own title", required=False
+        help_text="Leave blank to use the page's own title, required if using a URL",
+        required=False,
     )
 
-    class Meta:
-        template = ("patterns/molecules/navigation/blocks/menu_item.html",)
+    def clean(self, value):
+        try:
+            result = super().clean(value)
+        except ValidationError as e:
+            errors = e.params
+        else:
+            errors = {}
+
+        if (value["url"] and value["page"]) or (not value["url"] and not value["page"]):
+            errors["url"] = ErrorList(["Please select a URL or a Page"])
+        if value["url"] and not value["title"]:
+            errors["url"] = ErrorList(["Please set a title when using a URL"])
+
+        if errors:
+            raise ValidationError("Validation error in LinkBlock", params=errors)
+        return result
 
 
 class LinkColumnWithHeader(blocks.StructBlock):
@@ -25,14 +43,28 @@ class LinkColumnWithHeader(blocks.StructBlock):
         template = ("patterns/molecules/navigation/blocks/footer_column.html",)
 
 
+class SecondaryLinkBlock(LinkBlock):
+    tertiary_links = blocks.ListBlock(LinkBlock())
+
+
+class QuickLinkBlock(LinkBlock):
+    sub_text = blocks.CharBlock(required=False)
+
+
+class PrimaryNavLink(blocks.StructBlock):
+    primary_link = LinkBlock(label="Top level menu links")
+    secondary_links = blocks.ListBlock(SecondaryLinkBlock())
+
+
 @register_setting(icon="list-ul")
 class NavigationSettings(BaseSetting, ClusterableModel):
+
     primary_navigation = StreamField(
-        [("link", LinkBlock())], blank=True, help_text="Main site navigation"
+        [("link", PrimaryNavLink())], blank=True, help_text="Main site navigation"
     )
-    secondary_navigation = StreamField(
-        [("link", LinkBlock())], blank=True, help_text="Alternative navigation"
-    )
+
+    quick_links = StreamField([("link", QuickLinkBlock())], blank=True)
+
     footer_navigation = StreamField(
         [("column", LinkColumnWithHeader())],
         blank=True,
@@ -45,8 +77,8 @@ class NavigationSettings(BaseSetting, ClusterableModel):
     )
 
     panels = [
+        StreamFieldPanel("quick_links"),
         StreamFieldPanel("primary_navigation"),
-        StreamFieldPanel("secondary_navigation"),
         StreamFieldPanel("footer_navigation"),
         StreamFieldPanel("footer_links"),
     ]
--- rca/project_styleguide/templatetags/navigation_tags.py
+++ rca/project_styleguide/templatetags/navigation_tags.py
@@ -3,7 +3,7 @@ from pattern_library.monkey_utils import override_tag
 from rca.navigation.templatetags.navigation_tags import register
 
 override_tag(register, name="primarynav")
-override_tag(register, name="secondarynav")
 override_tag(register, name="footernav")
+override_tag(register, name="audience_links")
 override_tag(register, name="sidebar")
 override_tag(register, name="footerlinks")
