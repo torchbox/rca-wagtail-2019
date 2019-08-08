e539c4f5a2a0af8e9ad655ad7f690318ad0f3ba4
Chris Lawton <chris.lawton@torchbox.com>
2019-08-08 13:27:12 +0100

    get the back link working

--- rca/static_src/javascript/components/back-link.js
+++ rca/static_src/javascript/components/back-link.js
@@ -8,17 +8,33 @@ class BackLink {
     constructor(node) {
         this.node = node;
         this.activeClass = 'is-visible';
-        this.levelTwo = document.querySelector('[data-nav-level-two]');
-        this.levelThree = document.querySelector('[data-nav-level-three]');
-        new ClassWatcher(this.levelTwo, this.activeClass, this.onClassAdd.bind(this), this.onClassRemoval);
-        new ClassWatcher(this.levelThree, this.activeClass, this.onClassAdd.bind(this), this.onClassRemoval);
+        this.levelTwo = document.querySelector('[data-nav-level-2]');
+        this.levelThree = document.querySelector('[data-nav-level-3]');
+        this.levelTwoMenus = document.querySelectorAll('[data-nav-level-2] ul');
+        this.levelThreeMenus = document.querySelectorAll('[data-nav-level-3] ul');
+        new ClassWatcher(this.levelTwo, this.activeClass, this.onClassAdd.bind(this), this.onClassRemove.bind(this));
+        new ClassWatcher(this.levelThree, this.activeClass, this.onClassAdd.bind(this), this.onClassRemove.bind(this));
 
         this.bindEventListeners();
     }
 
     bindEventListeners() {
         this.node.addEventListener('click', () => {
-            console.log('hello');
+            // check if level 3 is open first
+            if(this.levelThree.classList.contains(this.activeClass)) {
+                this.levelThree.classList.remove(this.activeClass);
+                this.hideMenus(this.levelThreeMenus);
+            } else {
+                this.node.classList.remove(this.activeClass);
+                this.levelTwo.classList.remove(this.activeClass);
+                this.hideMenus(this.levelTwoMenus);
+            }
+        });
+    }
+
+    hideMenus(menus) {
+        menus.forEach(menu => {
+            menu.classList.remove(this.activeClass);
         });
     }
 
@@ -26,8 +42,8 @@ class BackLink {
         this.node.classList.add(this.activeClass);
     }
 
-    onClassRemoval() {
-        this.node.classList.remove(this.activeClass);
+    onClassRemove() {
+        return;
     }
 }
 
