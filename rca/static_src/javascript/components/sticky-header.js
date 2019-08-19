caad79bdaf4ecabe1a39795c0baa9886f2725564
nicklee <nick@nicklee.co.uk>
2019-08-19 16:33:27 +0100

    Initial sticky nav work

--- rca/static_src/javascript/components/sticky-header.js
+++ rca/static_src/javascript/components/sticky-header.js
@@ -2,32 +2,66 @@ import headRoom from 'headroom.js';
 
 function StickyHeader() {
     let elem = document.body;
-    let options = {
-        offset: 200,
-        tolerance: {
-            up: 10,
-            down: 5,
-        },
-        classes: {
-            // when element is initialised
-            initial: 'headroom',
-            // when scrolling up
-            pinned: 'headroom--pinned',
-            // when scrolling down
-            unpinned: 'headroom--unpinned',
-            // when above offset
-            top: 'headroom--top',
-            // when below offset
-            notTop: 'headroom--not-top',
-            // when at bottom of scoll area
-            bottom: 'headroom--bottom',
-            // when not at bottom of scroll area
-            notBottom: 'headroom--not-bottom',
-        },
-    };
+    var intViewportHeight = window.innerHeight;
+    var offSetAdjuster = 10;
+    var customOffet = intViewportHeight + offSetAdjuster;
 
-    let headroom = new headRoom(elem, options);
-    headroom.init();
+    if(document.body.contains(document.querySelector('.app--homepage'))){
+        let options = {
+            offset: customOffet,
+            tolerance: {
+                up: 10,
+                down: 20,
+            },
+            classes: {
+                // when element is initialised
+                initial: 'headroom',
+                // when scrolling up
+                pinned: 'headroom--pinned',
+                // when scrolling down
+                unpinned: 'headroom--unpinned',
+                // when above offset
+                top: 'headroom--top',
+                // when below offset
+                notTop: 'headroom--not-top',
+                // when at bottom of scoll area
+                bottom: 'headroom--bottom',
+                // when not at bottom of scroll area
+                notBottom: 'headroom--not-bottom',
+            },
+        };
+
+        let headroom = new headRoom(elem, options);
+        headroom.init();
+    } else {
+        let options = {
+            offset: 200,
+            tolerance: {
+                up: 10,
+                down: 5,
+            },
+            classes: {
+                // when element is initialised
+                initial: 'headroom',
+                // when scrolling up
+                pinned: 'headroom--pinned',
+                // when scrolling down
+                unpinned: 'headroom--unpinned',
+                // when above offset
+                top: 'headroom--top',
+                // when below offset
+                notTop: 'headroom--not-top',
+                // when at bottom of scoll area
+                bottom: 'headroom--bottom',
+                // when not at bottom of scroll area
+                notBottom: 'headroom--not-bottom',
+            },
+        };
+
+        let headroom = new headRoom(elem, options);
+        headroom.init();
+    }
+
 }
 
 StickyHeader();
