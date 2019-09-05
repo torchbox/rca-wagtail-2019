79eb0ac26014a3599a50aaec7268920e46e8c5fd
Karl Hobley <karl@kaed.uk>
2019-09-05 15:30:03 +0100

    isort

--- rca/asgi.py
+++ rca/asgi.py
@@ -1,8 +1,8 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from content_locking import routing
 from django.urls import path
 
-from content_locking import routing
 
 application = ProtocolTypeRouter(
     {"websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))}
