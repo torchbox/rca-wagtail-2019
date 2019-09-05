f94626f3c9781364e1648a9e8ff885d1bbbebf09
Karl Hobley <karl@kaed.uk>
2019-09-05 15:33:29 +0100

    Remove blank line

--- rca/asgi.py
+++ rca/asgi.py
@@ -3,7 +3,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from content_locking import routing
 from django.urls import path
 
-
 application = ProtocolTypeRouter(
     {"websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))}
 )
