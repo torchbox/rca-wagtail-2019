80473838e3ac9c15e39a18cdd9b5f07364665f0a
kevinhowbrook <kbhowbrook@gmail.com>
2019-09-11 10:39:39 +0100

    Whitelist torchbox staging site to allow proxy without basic auth

--- rca/settings/base.py
+++ rca/settings/base.py
@@ -591,6 +591,7 @@ if env.get("BASIC_AUTH_ENABLED", "false").lower().strip() == "true":
         "89.197.53.244/30",
         "193.227.244.0/23",
         "2001:41c8:103::/48",
+        "193.227.244.54",
     ]
 
     # This is the list of hosts that website can be accessed without basic auth
