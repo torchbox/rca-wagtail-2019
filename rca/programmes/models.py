de2a593e0a0d05b8715ecaa94dba05d92e57ea6d
kevinhowbrook <kbhowbrook@gmail.com>
2019-09-11 14:43:45 +0100

    Fixes to info and accordion blocks

--- rca/programmes/models.py
+++ rca/programmes/models.py
@@ -314,7 +314,7 @@ class ProgrammePage(BasePage):
     scholarships_title = models.CharField(max_length=120)
     scholarships_information = models.CharField(max_length=250)
     scholarship_accordion_items = StreamField(
-        [("accodrion", AccordionBlockWithTitle())], blank=True
+        [("accordion", AccordionBlockWithTitle())], blank=True
     )
     scholarship_information_blocks = StreamField(
         [("information_block", InfoBlock())], blank=True
