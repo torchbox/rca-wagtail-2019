a39db215ff20addb0216833f2032812529ffca47
kevinhowbrook <kbhowbrook@gmail.com>
2020-01-02 17:13:34 +0000

    Override the search filter to implement 'autocompleting' on specified fields.

--- rca/wagtailapi/filters.py
+++ rca/wagtailapi/filters.py
@@ -1,4 +1,9 @@
+from django.conf import settings
 from rest_framework import filters
+from rest_framework.filters import BaseFilterBackend
+from wagtail.api.v2.utils import BadRequestError
+from wagtail.search.backends import get_search_backend
+from wagtail.search.backends.base import FilterFieldError, OrderByFieldError
 
 
 class DegreeLevelFilter(filters.BaseFilterBackend):
@@ -11,5 +16,51 @@ class DegreeLevelFilter(filters.BaseFilterBackend):
 
         if pks:
             queryset = queryset.filter(degree_level__in=pks)
+
+        return queryset
+
+
+class SearchFilter(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        """
+        This performs a full-text search on the result set
+        Eg: ?search=James Joyce
+        """
+        search_enabled = getattr(settings, "WAGTAILAPI_SEARCH_ENABLED", True)
+
+        if "search" in request.GET:
+            if not search_enabled:
+                raise BadRequestError("search is disabled")
+
+            # Searching and filtering by tag at the same time is not supported
+            if getattr(queryset, "_filtered_by_tag", False):
+                raise BadRequestError(
+                    "filtering by tag with a search query is not supported"
+                )
+
+            search_query = request.GET["search"]
+            search_operator = request.GET.get("search_operator", None)
+            order_by_relevance = "order" not in request.GET
+
+            sb = get_search_backend()
+            try:
+                queryset = sb.autocomplete(
+                    search_query,
+                    queryset,
+                    operator=search_operator,
+                    order_by_relevance=order_by_relevance,
+                )
+            except FilterFieldError as e:
+                raise BadRequestError(
+                    "cannot filter by '{}' while searching (field is not indexed)".format(
+                        e.field_name
+                    )
+                )
+            except OrderByFieldError as e:
+                raise BadRequestError(
+                    "cannot order by '{}' while searching (field is not indexed)".format(
+                        e.field_name
+                    )
+                )
 
         return queryset
