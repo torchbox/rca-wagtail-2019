056768d8491ae77b8ae650a1fb4cb5ba806fa4c4
Thibaud Colas <thibaudcolas@gmail.com>
2020-01-07 16:50:15 +0000

    Improve category panels display

--- rca/static_src/javascript/programmes/components/ProgrammesCategories/CategoriesPanels.js
+++ rca/static_src/javascript/programmes/components/ProgrammesCategories/CategoriesPanels.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 
 import { programmeCategories } from '../../programmes.types';
 
-import Icon from '../Icon/Icon';
+import CategoryItem from './CategoryItem';
 
-const CategoriesPanels = ({ categories, activeCategory, applyFilter }) => {
+const CategoriesPanels = ({ categories, activeCategory }) => {
     return (
         <div className="categories-panels bg bg--light">
             {categories.map((c) => (
@@ -18,33 +18,8 @@ const CategoriesPanels = ({ categories, activeCategory, applyFilter }) => {
                     aria-expanded={c.id === activeCategory}
                     hidden={c.id !== activeCategory}
                 >
-                    {c.items.map((i) => (
-                        <button
-                            className="categories-panels__panel-btn grid"
-                            type="button"
-                            key={i.id}
-                            onClick={() => {
-                                const params = new URLSearchParams(
-                                    window.location.search,
-                                );
-                                params.set(c.id, i.id);
-                                console.log(params);
-                                window.history.pushState(
-                                    null,
-                                    null,
-                                    `${window.location.pathname}?${params}`,
-                                );
-                                applyFilter({
-                                    [c.id]: i.id,
-                                });
-                            }}
-                        >
-                            <h3 className="heading heading--three">
-                                {i.title}
-                            </h3>
-                            <p>{i.description}</p>
-                            <Icon name="arrow-right" />
-                        </button>
+                    {c.items.map((item) => (
+                        <CategoryItem key={item.id} category={item} />
                     ))}
                 </div>
             ))}
