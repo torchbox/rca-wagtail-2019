b2227e443031795f806f5dc170c51ed47718e183
kevinhowbrook <kbhowbrook@gmail.com>
2020-01-09 11:49:11 +0000

    Wrong version of black locally, rerun

--- rca/settings/base.py
+++ rca/settings/base.py
@@ -101,7 +101,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
-    ),
+    )
 }
 
 # Middleware classes
