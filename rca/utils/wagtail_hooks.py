7da86109922b7036625eb331a90f2fa4cc1a1052
kevinhowbrook <kbhowbrook@gmail.com>
2020-01-15 10:47:30 +0000

    Implements new taxonomy and schools model for programme page listing filters

--- rca/utils/wagtail_hooks.py
+++ rca/utils/wagtail_hooks.py
@@ -4,11 +4,16 @@ from wagtail.contrib.modeladmin.options import (
     modeladmin_register,
 )
 
-from rca.programmes.models import DegreeLevel, ProgrammeType
+from rca.programmes.models import DegreeLevel, ProgrammeType, Subject
 
 
 class DegreeLevelModelAdmin(ModelAdmin):
     model = DegreeLevel
+    menu_icon = "tag"
+
+
+class SubjectModelAdmin(ModelAdmin):
+    model = Subject
     menu_icon = "tag"
 
 
@@ -19,7 +24,7 @@ class ProgrammeTypeModelAdmin(ModelAdmin):
 
 class TaxonomiesModelAdminGroup(ModelAdminGroup):
     menu_label = "Taxonomies"
-    items = (DegreeLevelModelAdmin, ProgrammeTypeModelAdmin)
+    items = (DegreeLevelModelAdmin, ProgrammeTypeModelAdmin, SubjectModelAdmin)
     menu_icon = "tag"
 
 
