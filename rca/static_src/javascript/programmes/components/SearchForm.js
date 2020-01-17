a46e40350de372a1773ceb8a87c1d01a8ab2f5ec
Thibaud Colas <thibaudcolas@gmail.com>
2020-01-17 08:05:13 +0000

    Remove unused import

--- rca/static_src/javascript/programmes/components/SearchForm.js
+++ rca/static_src/javascript/programmes/components/SearchForm.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import debounce from 'lodash.debounce';
