d74a226b79b04b59443cc461bdac482928471b49
kevinhowbrook <kbhowbrook@gmail.com>
2020-01-20 12:59:23 +0000

    Add a menu item so the new settings can be reached

--- rca/api_content/wagtail_hooks.py
+++ rca/api_content/wagtail_hooks.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+from wagtail.admin.menu import MenuItem
 from wagtail.core import hooks
 
 from .views import ApiContentCacheClear, ApiContentCacheSettings
@@ -7,6 +10,18 @@ from .views import ApiContentCacheClear, ApiContentCacheSettings
 @hooks.register("register_admin_urls")
 def urlconf_time():
     return [
-        url(r"^api-cache/", ApiContentCacheSettings.as_view()),
+        url(
+            r"^api-cache/", ApiContentCacheSettings.as_view(), name="api-cache-settings"
+        ),
         url(r"^api-cache/clear/", ApiContentCacheClear),
     ]
+
+
+@hooks.register("register_settings_menu_item")
+def register_api_content_settings_menu_item():
+    return MenuItem(
+        _("API content settings"),
+        reverse("api-cache-settings"),
+        classnames="icon icon-cog",
+        order=1000,
+    )
