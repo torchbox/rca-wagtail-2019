4b18d13ecda4e1582ccdce83022be7b16c1641d4
kevinhowbrook <kbhowbrook@gmail.com>
2020-03-30 09:47:24 +0100

    Set the xml if no exception is raised

--- rca/shortcourses/models.py
+++ rca/shortcourses/models.py
@@ -15,7 +15,6 @@ from rca.utils.models import BasePage
 
 
 class ShortCoursePage(BasePage):
-    parent_page_types = ["guides.GuidePage"]
     template = "patterns/pages/shortcourses/short_course.html"
 
     hero_image = models.ForeignKey(
