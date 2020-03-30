4c0e95153529dca6aa58a2436298a9a6e5899355
kevinhowbrook <kbhowbrook@gmail.com>
2020-03-30 12:15:34 +0100

    Update page location rules

--- rca/shortcourses/models.py
+++ rca/shortcourses/models.py
@@ -2,5 +2,5 @@ from rca.utils.models import BasePage
 
 
 class ShortCoursePage(BasePage):
-    parent_page_types = ["guides.GuidePage"]
+    parent_page_types = ["programmes.ProgrammeIndexPage"]
     template = "patterns/pages/shortcourses/short_course.html"
