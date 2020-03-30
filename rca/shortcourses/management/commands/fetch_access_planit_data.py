4b135575426b6227bbf8716d011014827e339757
kevinhowbrook <kbhowbrook@gmail.com>
2020-03-30 10:34:14 +0100

    This no longer needs a try/except because we are only updating courses _with_ course ids

--- rca/shortcourses/management/commands/fetch_access_planit_data.py
+++ rca/shortcourses/management/commands/fetch_access_planit_data.py
@@ -1,6 +1,5 @@
 import logging
 
-from django.core.exceptions import ValidationError
 from django.core.management import BaseCommand
 
 from rca.shortcourses.access_planit import AccessPlanitXML
@@ -15,11 +14,8 @@ class Command(BaseCommand):
     def handle(self, **options):
         short_courses = ShortCoursePage.objects.exclude(access_planit_course_id="")
         for course in short_courses:
-            try:
-                logger.info(
-                    f"Fetching XML for course_id:{course.id} with AccessPlanit ID:{course.access_planit_course_id}"
-                )
-                ap_data = AccessPlanitXML(course_id=course.access_planit_course_id)
-                ap_data.set_data_in_cache()
-            except ValidationError as e:
-                logger.exception(f"Failed to fetch XML from AccessPlanit {e}")
+            logger.info(
+                f"Fetching XML for course_id:{course.id} with AccessPlanit ID:{course.access_planit_course_id}"
+            )
+            ap_data = AccessPlanitXML(course_id=course.access_planit_course_id)
+            ap_data.set_data_in_cache()
