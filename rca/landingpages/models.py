0bcc435aff65b9790360c2085f912c1ea2d0be4c
kevinhowbrook <kbhowbrook@gmail.com>
2020-04-08 13:19:19 +0100

    Shift landing page to it's own app as there are now 3 separate templates

---
+++ rca/landingpages/models.py
@@ -0,0 +1,20 @@
+from rca.utils.models import BasePage
+
+
+class LandingPage(BasePage):
+    template = "patterns/pages/landingpage/landing_page.html"
+
+    class Meta:
+        abstract = True
+
+
+class ResearchLandingPage(LandingPage):
+    template = "patterns/pages/landingpage/landing_page.html"
+
+
+class InovationLandingPage(LandingPage):
+    template = "patterns/pages/landingpage/landing_page.html"
+
+
+class EnterpriseLandingPage(LandingPage):
+    template = "patterns/pages/landingpage/landing_page.html"
