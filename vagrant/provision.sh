458ca9121124ed8b6461f78cb0c4cd3a7b341897
Storm Heg <storm.heg@torchbox.com>
2020-04-15 10:20:53 +0200

    Upgrade Postgres to 12

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -16,8 +16,8 @@ apt-get update -y
 # Upgrade to postgres 11
 service postgresql stop
 apt-get remove -y --purge postgresql-*
-apt-get install -y postgresql-11 postgresql-client-11 postgresql-contrib-11
-pg_ctlcluster 11 main start
+apt-get install -y postgresql-12 postgresql-client-12 postgresql-contrib-12
+pg_ctlcluster 12 main start
 su - postgres -c "createuser -s vagrant"
 
 
