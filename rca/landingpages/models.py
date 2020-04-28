2cf457d2053d51ec0d09a30ca38c87d8625c040b
kevinhowbrook <kbhowbrook@gmail.com>
2020-04-28 15:43:14 +0100

    Rework the related models for better inheritence so we don't end up creating tonnes of interim models

--- rca/landingpages/models.py
+++ rca/landingpages/models.py
@@ -30,9 +30,6 @@ class FeaturedImage(LinkFields):
     subtitle = models.TextField(max_length=120, blank=True)
     description = models.TextField(max_length=250, blank=True)
 
-    class Meta:
-        abstract = True
-
     panels = [
         FieldPanel("title"),
         ImageChooserPanel("image"),
@@ -40,20 +37,41 @@ class FeaturedImage(LinkFields):
         FieldPanel("description"),
     ] + LinkFields.panels
 
+    class Meta:
+        abstract = True
 
-class LandingPageHighlightLinkField(LinkFields):
-    source_page = ParentalKey("LandingPage", related_name="highlight_link")
+
+class LandingPageFeaturedImage(FeaturedImage):
+    source_page = ParentalKey("LandingPage", related_name="featured_image")
 
 
-class LandingPageAboutLinkField(LinkFields):
-    source_page = ParentalKey("LandingPage", related_name="about_page")
+class LandingPageRelatedPagegrid(RelatedPage):
+    source_page = ParentalKey(
+        "landingpages.LandingPage", related_name="related_pages_grid"
+    )
+    panels = [PageChooserPanel("page")]
+
+
+class LandingPageRelatedPageHighlights(RelatedPage):
+    source_page = ParentalKey(
+        "landingpages.LandingPage", related_name="related_pages_highlights"
+    )
+    panels = [PageChooserPanel("page")]
+
+
+class LandingPageRelatedPageList(RelatedPage):
+    source_page = ParentalKey(
+        "landingpages.LandingPage", related_name="related_pages_list"
+    )
+    panels = [PageChooserPanel("page")]
 
 
 class LandingPage(BasePage):
-    """ Defines all the fields we will need for the other versions of landing pages"""
+    """ Defines all the fields we will need for the other versions of landing pages
+    visibility of some extra fields that aren't needed on certain models which inherit LandingPage
+    are controlled at the content_panels level"""
 
-    # TODO Make this createable and act as the default
-    is_creatable = False
+    template = "patterns/pages/landingpage/landing_page.html"
     hero_image = models.ForeignKey(
         "images.CustomImage",
         null=True,
@@ -63,7 +81,43 @@ class LandingPage(BasePage):
     )
     hero_colour_option = models.PositiveSmallIntegerField(choices=(HERO_COLOUR_CHOICES))
     introduction = models.TextField(max_length=500, blank=True)
+    about_page = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
     highlights_title = models.TextField(max_length=80, blank=True)
+    highlights_link_url = models.URLField(
+        blank=True, help_text=("Optional link to more information")
+    )
+    highlights_link_text = models.CharField(
+        max_length=80, blank=True, help_text=_("The text to display for the link")
+    )
+
+    related_pages_title = models.TextField(
+        max_length=80,
+        blank=True,
+        help_text=_("The title to be displayed above the related pages grid"),
+    )
+    related_pages_text = models.TextField(
+        max_length=250,
+        blank=True,
+        help_text=_(
+            "The brief paragraph of text to be displayed above the related pages grid"
+        ),
+    )
+    related_pages_list_title = models.TextField(
+        max_length=80,
+        blank=True,
+        help_text=_("The title to be displayed above the related pages"),
+    )
+    related_pages_list_subtitle = models.TextField(
+        max_length=80,
+        blank=True,
+        help_text=_("The subtitle, displayed above the related pages"),
+    )
 
     content_panels = BasePage.content_panels + [
         MultiFieldPanel(
@@ -71,11 +125,39 @@ class LandingPage(BasePage):
             heading=_("Hero"),
         ),
         MultiFieldPanel(
-            [
-                FieldPanel("introduction"),
-                InlinePanel("about_page", label=_("About page")),
-            ],
+            [FieldPanel("introduction"), PageChooserPanel("about_page")],
             heading=_("Course Introduction"),
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("highlights_title"),
+                InlinePanel("related_pages_highlights", label=_("Page"), max_num=8),
+                FieldPanel("highlights_link_url"),
+                FieldPanel("highlights_link_text"),
+            ],
+            heading=_("Highlight pages carousel"),
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("related_pages_title"),
+                FieldPanel("related_pages_text"),
+                InlinePanel("related_pages_grid", max_num=8, label=_("Related Pages"),),
+            ],
+            heading=_("Related pages grid"),
+        ),
+        InlinePanel("featured_image", label=_("Featured image"), max_num=1),
+        MultiFieldPanel(
+            [
+                FieldPanel("related_pages_list_title"),
+                FieldPanel("related_pages_list_subtitle"),
+                InlinePanel(
+                    "related_pages_list", label=_("Related programmes"), max_num=8,
+                ),
+                # InlinePanel(
+                #     "related_programmes_link", label=_("View more link"), max_num=1
+                # ),
+            ],
+            heading=_("Related programmes"),
         ),
     ]
 
@@ -102,123 +184,9 @@ class LandingPage(BasePage):
         if hasattr(self, "featured_image"):
             return self.featured_image.first
 
-    def get_context(self, request, *args, **kwargs):
-        context = super().get_context(request, *args, **kwargs)
-        context["hero_colour"] = DARK_HERO
-        if int(self.hero_colour_option) == LIGHT_TEXT_ON_DARK_IMAGE:
-            context["hero_colour"] = LIGHT_HERO
-        context["about_page"] = self.about_page.first
-        context["featured_image"] = self.get_featured_image()
-
-        return context
-
-
-class LandingPageDefaultFeaturedImage(FeaturedImage):
-    # TODO change to foreign key
-    source_page = ParentalKey(
-        "landingpages.LandingPageDefault", related_name="featured_image"
-    )
-
-
-class LandingPageDefaultHighlightPages(RelatedPage):
-    source_page = ParentalKey(
-        "landingpages.LandingPageDefault", related_name="highlight_pages_default"
-    )
-    panels = [PageChooserPanel("page", ["projects.ProjectPage"])]
-
-
-class LandingPageDefaultRelatedPages(RelatedPage):
-    # TODO source page should be to LandingPage
-    source_page = ParentalKey(
-        "landingpages.LandingPageDefault", related_name="related_pages"
-    )
-    panels = [PageChooserPanel("page")]
-
-
-class LandingPageDefaultRelatedProgrammesLinkField(LinkFields):
-    source_page = ParentalKey(
-        "LandingPageDefault", related_name="related_programmes_link"
-    )
-
-
-class LandingPageRelatedProgrammPages(RelatedPage):
-    source_page = ParentalKey(
-        "landingpages.LandingPage", related_name="related_programmes"
-    )
-    panels = [
-        PageChooserPanel(
-            "page", ["programmes.ProgrammePage", "shortcourses.ShortCoursePage"]
-        )
-    ]
-
-
-class LandingPageDefault(LandingPage):
-    template = "patterns/pages/landingpage/landing_page.html"
-
-    class Meta:
-        verbose_name = "Landing Page - Default"
-
-    related_pages_title = models.TextField(
-        max_length=80,
-        blank=True,
-        help_text=_("The title to be displayed above the related pages grid"),
-    )
-    related_pages_text = models.TextField(
-        max_length=250,
-        blank=True,
-        help_text=_(
-            "The brief paragraph of text to be displayed above the related pages grid"
-        ),
-    )
-    related_programmes_title = models.TextField(
-        max_length=80,
-        blank=True,
-        help_text=_("The title to be displayed above the related pages"),
-    )
-    related_programmes_subtitle = models.TextField(
-        max_length=80,
-        blank=True,
-        help_text=_("The subtitle, displayed above the related pages"),
-    )
-
-    content_panels = LandingPage.content_panels + [
-        MultiFieldPanel(
-            [
-                FieldPanel("highlights_title"),
-                InlinePanel(
-                    "highlight_pages_default", max_num=8, label=_("Highlight pages")
-                ),
-                InlinePanel("highlight_link", max_num=1, label=_("Read more link")),
-            ],
-            heading=_("Highlight pages carousel"),
-        ),
-        MultiFieldPanel(
-            [
-                FieldPanel("related_pages_title"),
-                FieldPanel("related_pages_text"),
-                InlinePanel("related_pages", label=_("Related pages"), max_num=8),
-            ],
-            heading=_("Related pages"),
-        ),
-        InlinePanel("featured_image", label=_("Featured image"), max_num=1),
-        MultiFieldPanel(
-            [
-                FieldPanel("related_programmes_title"),
-                FieldPanel("related_programmes_subtitle"),
-                InlinePanel(
-                    "related_programmes", label=_("Related programmes"), max_num=8
-                ),
-                InlinePanel(
-                    "related_programmes_link", label=_("View more link"), max_num=1
-                ),
-            ],
-            heading=_("Related programmes"),
-        ),
-    ]
-
-    def get_related_pages(self):
+    def get_related_pages(self, pages):
         related_pages = []
-        for value in self.related_pages.select_related("page"):
+        for value in pages.select_related("page"):
             if value.page and value.page.live:
                 page = value.page.specific
                 related_pages.append(
@@ -237,17 +205,22 @@ class LandingPageDefault(LandingPage):
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
-        context["highlight_pages"] = self._format_projects_for_gallery(
-            self.highlight_pages_default.all()
+        context["hero_colour"] = DARK_HERO
+        if int(self.hero_colour_option) == LIGHT_TEXT_ON_DARK_IMAGE:
+            context["hero_colour"] = LIGHT_HERO
+        context["about_page"] = self.about_page
+        context["related_pages_highlights"] = self.get_related_pages(
+            self.related_pages_highlights
         )
-        context["related_pages"] = self.get_related_pages()
-        context["related_programmes"] = [
+        context["related_pages"] = self.get_related_pages(self.related_pages_grid)
+        context["featured_image"] = self.get_featured_image()
+        context["related_pages_list"] = [
             {
-                "title": self.related_programmes_title,
-                "subtitle": self.related_programmes_subtitle,
+                "title": self.related_pages_list_title,
+                "subtitle": self.related_pages_list_subtitle,
                 "related_items": [
                     rel.page.specific
-                    for rel in self.related_programmes.select_related("page")
+                    for rel in self.related_pages_list.select_related("page")
                 ],
             }
         ]
@@ -257,6 +230,9 @@ class LandingPageDefault(LandingPage):
 
 class ResearchLandingPage(LandingPage):
     template = "patterns/pages/landingpage/landing_page--research.html"
+    content_panels = [
+        InlinePanel("featured_image", label=_("Featured image"), max_num=1),
+    ]
 
     class Meta:
         verbose_name = "Landing Page - Research"
