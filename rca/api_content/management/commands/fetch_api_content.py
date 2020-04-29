86083264feb1c632369ed6070c351e60cf89308c
Rich Brennan <rich.brennan@torchbox.com>
2020-04-29 14:08:15 +0100

    refactor alumni stories and news/events to use a management command to fetch from api

---
+++ rca/api_content/management/commands/fetch_api_content.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+
+from ...content import AlumniStoriesAPI, NewsEventsAPI
+
+
+class Command(BaseCommand):
+    """Fetch content from the legacy site and store in the cache """
+    def handle(self, *args, **options):
+        for api in (AlumniStoriesAPI, NewsEventsAPI):
+            api().fetch_from_api()
