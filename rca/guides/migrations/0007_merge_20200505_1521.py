59ae2d760555fbb7e0de6de16d3821a09c1b8d8e
nicklee <nick@nicklee.co.uk>
2020-05-05 17:25:37 +0100

    Running black

--- rca/guides/migrations/0007_merge_20200505_1521.py
+++ rca/guides/migrations/0007_merge_20200505_1521.py
@@ -5,8 +5,6 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ("guides", "0006_extra_title_field"),
-    ]
+    dependencies = [("guides", "0006_extra_title_field")]
 
     operations = []
