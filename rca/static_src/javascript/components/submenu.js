a15ce182da8580296e8a4ee0d88e1302db4cad0d
Chris Lawton <chris.lawton@torchbox.com>
2020-05-21 10:18:45 +0100

    move focus to menu and child menus when navigating

--- rca/static_src/javascript/components/submenu.js
+++ rca/static_src/javascript/components/submenu.js
@@ -88,6 +88,10 @@ class SubMenu {
         const childMenuSelector = `[data-menu-${navItem.dataset.menuId}]`;
         const childMenu = this.addClass(childMenuSelector, this.visibleClass);
 
+        // Move the focus to the fisrt <a> in a child menu that was opened
+        const childMenuElement = childMenu[0];
+        childMenuElement.querySelector('a:not(.nav__link--group-heading)').focus();
+
         // only show child drawer if has a menu else make sure it's gone
         if (childMenu.length > 0) {
             this.addClass(childDrawer, this.visibleClass);
