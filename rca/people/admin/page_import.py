7f3c6f55e99f452254e348ba26a49142ac6b1b78
Andy Babic <andy.babic@torchbox.com>
2020-06-08 09:29:55 +0100

    Tidy up implementation

--- rca/people/admin/page_import.py
+++ rca/people/admin/page_import.py
@@ -1,5 +1,7 @@
+from django.contrib.contenttypes.models import ContentType
 from import_export import resources
 from import_export.admin import ImportMixin
+from wagtail.core.models import Page
 
 from .page_import_forms import PageConfirmImportForm, PageImportForm
 
@@ -25,14 +27,37 @@ class PageImportMixin(ImportMixin):
         init_kwargs["owner"] = form_data.get("owner")
         return init_kwargs
 
+    def get_parent_page_queryset(self):
+        allowed_content_types = list(
+            ContentType.objects.get_for_models(
+                *self.model.allowed_parent_page_models()
+            ).values()
+        )
+        return Page.objects.filter(content_type__in=allowed_content_types, depth__gt=1)
+
     def get_form_kwargs(self, form, *args, **kwargs):
-        form_kwargs = super().get_form_kwargs(form, *args, **kwargs)
-        form_kwargs.update(admin_site=self.admin_site, page_type=self.model)
+        vals = super().get_form_kwargs(form, *args, **kwargs)
         if isinstance(form, PageImportForm):
-            initial_vals = form_kwargs.get("initial", {})
-            initial_vals["owner"] = self.request.user.id
-            form_kwargs["initial"] = initial_vals
-        return form_kwargs
+            # The method is just being used to prep the 'initial data' for the
+            # 'confirm' form, so pass through the values from the 'import' form.
+            vals.update(
+                parent_page=form.cleaned_data["parent_page"],
+                owner=form.cleaned_data["owner"],
+            )
+            return vals
+
+        # The method is being used to prepare full 'init' values for the 'import'
+        # form. So, add additional required values, as well as adding some helpful
+        # initial data values
+        parent_page_queryset = self.get_parent_page_queryset()
+        vals.update(
+            admin_site=self.admin_site, parent_page_queryset=parent_page_queryset
+        )
+        initial_vals = vals.get("initial", {})
+        initial_vals["parent_page"] = parent_page_queryset.first()
+        initial_vals["owner"] = self.request.user.id
+        vals["initial"] = initial_vals
+        return vals
 
 
 class PageResource(resources.ModelResource):
@@ -47,17 +72,27 @@ class PageResource(resources.ModelResource):
         self.owner = owner
         super().__init__(**kwargs)
 
+    def after_import_row(self, row, row_result, **kwargs):
+        """
+        If diffing is disabled (required for pages, due to a pickling issue),
+        Add a 'fake' diff to the row_result, so that row values are shown
+        in the template (otherwise it looks like all rows are blank).
+        """
+        if row_result.diff is None:
+            row_result.diff = [row.get(h, "") for h in self.get_diff_headers()]
+
     def save_instance(self, instance, using_transactions=True, dry_run=False):
         """
-        Takes care of saving the page to the database.
-        Keep in mind that this is done by calling ``instance.save()``, so
-        objects are not created in bulk!
+        Saves the page to the database
         """
         self.before_save_instance(instance, using_transactions, dry_run)
-        if not dry_run or using_transactions:
+        if not dry_run:
             if instance._state.adding:
-                # Add page to tree with the owner value set
+                # Set owner
                 instance.owner = self.owner
+                # Keep page as draft
+                instance.live = False
+                # Save the new page
                 self.parent_page.add_child(instance=instance)
             else:
                 # Update existing
@@ -70,5 +105,5 @@ class PageResource(resources.ModelResource):
         revision after saving the page itself.
         """
         super().after_save_instance(instance, using_transactions, dry_run)
-        if not dry_run or using_transactions:
+        if not dry_run:
             instance.save_revision(user=self.request.user)
--- rca/people/admin/page_import_forms.py
+++ rca/people/admin/page_import_forms.py
@@ -1,57 +1,42 @@
 from django import forms
+from django.contrib import admin
 from django.contrib.admin.widgets import AutocompleteSelect
 from django.contrib.auth import get_user_model
-from django.contrib.contenttypes.models import ContentType
 from import_export.forms import ConfirmImportForm, ImportForm
 from wagtail.core.models import Page
 
-
-class FakeRelation:
-    def __init__(self, model):
-        self.model = model
+User = get_user_model()
 
 
 class PageImportFormMixin(forms.Form):
     owner = forms.ModelChoiceField(
-        label="Owner (for new pages)", queryset=get_user_model().objects.all()
+        label="Owner (for new pages)", queryset=User.objects.all()
     )
     parent_page = forms.ModelChoiceField(
         label="Add new pages below", queryset=Page.objects.all()
     )
 
-    def __init__(self, *args, **kwargs):
-        self.admin_site = kwargs.pop("admin_site")
-        self.page_type = kwargs.pop("page_type")
-        super().__init__(*args, **kwargs)
-
-        # Limit parent_page queryset according to page_type
-        allowed_parent_page_content_types = list(
-            ContentType.objects.get_for_models(
-                *self.page_type.allowed_parent_page_models()
-            ).values()
-        )
-        qs = Page.objects.filter(
-            content_type__in=allowed_parent_page_content_types
-        ).exclude(depth=1)
-        self.fields["parent_page"].queryset = qs
-        self.initial["parent_page"] = qs.first()
-
 
 class PageImportForm(PageImportFormMixin, ImportForm):
-    autocomplete_fields = ("owner", "parent_page")
+    autocomplete_fields = ["owner", "parent_page"]
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, admin_site, parent_page_queryset=None, **kwargs):
         super().__init__(*args, **kwargs)
-        # Swap out standard select widgets for AutocompleteSelect
+
+        # Set 'parent_page' queryset to the provided one
+        if parent_page_queryset is not None:
+            self.fields["parent_page"].queryset = parent_page_queryset
+
+        # Replace standard modelchoice select widgets with
+        # AutocompleteSelect - which is much more efficient
+        # when the queryset is potentially quite large
         for field_name in self.autocomplete_fields:
             field = self.fields[field_name]
-            choices = field.widget.choices
-            rel = FakeRelation(field.queryset.model)
             field.widget = AutocompleteSelect(
-                rel,
-                admin_site=self.admin_site,
+                field.queryset,
+                admin_site,
                 using=field.queryset.db,
-                choices=choices,
+                choices=field.widget.choices,
             )
 
 
