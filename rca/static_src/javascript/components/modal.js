f9b7acfc7e79421013b6f2fabaf92ca20632bc47
Chris Lawton <chris.lawton@torchbox.com>
2020-06-09 16:01:00 +0100

    lint

--- rca/static_src/javascript/components/modal.js
+++ rca/static_src/javascript/components/modal.js
@@ -1,8 +1,12 @@
 import MicroModal from 'micromodal'; // es6 module
 
 MicroModal.init({
-    onShow: () => {window.location.hash = 'modal-open'},
-    onClose: () => {window.location.hash = ''},
+    onShow: () => {
+        window.location.hash = 'modal-open';
+    },
+    onClose: () => {
+        window.location.hash = '';
+    },
     awaitOpenAnimation: true,
     awaitCloseAnimation: true,
     disableScroll: true,
@@ -11,7 +15,8 @@ MicroModal.init({
 // check if the modal should be open on page load
 window.onload = () => {
     if (window.location.hash && window.location.hash === '#modal-open') {
-        const modalId = document.querySelector('[data-micromodal-trigger]').dataset.micromodalTrigger;
+        const modalId = document.querySelector('[data-micromodal-trigger]')
+            .dataset.micromodalTrigger;
         MicroModal.show(modalId);
     }
-}
+};
