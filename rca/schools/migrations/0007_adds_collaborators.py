8573b3bf7e1f52558755bb5aa8c65e7719d25052
kbh <kbhow@protonmail.com>
2021-01-08 17:50:23 +0000

    Fix migrations and template rendering

--- rca/schools/migrations/0007_adds_collaborators.py
+++ rca/schools/migrations/0007_adds_collaborators.py
@@ -9,7 +9,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("schools", "0008_adds_page_teasers"),
+        ("schools", "0006_adds_page_teasers"),
     ]
 
     operations = [
