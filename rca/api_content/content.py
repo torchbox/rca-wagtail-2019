c8e69d651f5fe5ffd1afb7cc8bc30ca32a7a2bf9
k <kbhow@protonmail.com>
2021-01-11 16:00:37 +0000

    Stop logging everything

--- rca/api_content/content.py
+++ rca/api_content/content.py
@@ -100,8 +100,11 @@ def parse_items_to_list(data, type):
 
         if "social_image" in data and data["social_image"]:
             social_image = data["social_image"]["meta"]["detail_url"]
-            social_image = requests.get(url=social_image, timeout=10)
-            social_image = social_image.json()
+            social_image = fetch_data(url=social_image)
+            try:
+                social_image = social_image.json()
+            except AttributeError:
+                continue
             if "url" in social_image["rca2019_feed_image"]:
                 social_image_url = social_image["rca2019_feed_image"]["url"]
                 social_image_small_url = social_image["rca2019_feed_image_small"]["url"]
