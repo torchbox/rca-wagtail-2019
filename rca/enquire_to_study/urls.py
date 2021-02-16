f6a3c439d328345fc70c3a3217eb8dc805768081
Brian Xu <brian.xu@torchbox.com>
2021-02-16 18:54:17 +0800

    configure router

---
+++ rca/enquire_to_study/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from rca.enquire_to_study.views import EnquireToStudyFormView, EnquireToStudyFormThanksView
+
+app_name = 'enquire_to_study'
+
+urlpatterns = [
+    path('', EnquireToStudyFormView.as_view(), name='enquire_to_study_form'),
+    path('thanks/', EnquireToStudyFormThanksView.as_view(), name='enquire_to_study_thanks'),
+]
