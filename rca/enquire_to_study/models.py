31c04ee9a7c86d4481ac60c519ea021ca2fd84bf
Brian Xu <brian.xu@torchbox.com>
2021-02-17 11:27:30 +0800

    create submission class

--- rca/enquire_to_study/models.py
+++ rca/enquire_to_study/models.py
@@ -3,26 +3,6 @@ from django_countries.fields import CountryField
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-class Programme(models.Model):
-    program = models.CharField(max_length=255)
-
-
-class Course(models.Model):
-    course = models.CharField(max_length=255)
-
-
-class StartDate(models.Model):
-    start_date = models.CharField(max_length=255)
-
-
-class Funding(models.Model):
-    funding = models.CharField(max_length=255)
-
-
-class InquiryReason(models.Model):
-    reason = models.CharField(max_length=255)
-
-
 class Submission(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -31,14 +11,10 @@ class Submission(models.Model):
     country_of_residence = CountryField()
     city = models.CharField(max_length=255)
     is_citizen = models.CharField(max_length=255)
-    programmes = models.ForeignKey(Programme, on_delete=models.CASCADE)
-    courses = models.ForeignKey(Course, on_delete=models.CASCADE)
-    start_date = models.OneToOneField(
-        StartDate,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
-    funding = models.ForeignKey(Funding, on_delete=models.CASCADE)
-    inquiry_reason = models.ForeignKey(InquiryReason, on_delete=models.CASCADE)
+    programmes = models.CharField(max_length=255)
+    courses = models.CharField(max_length=255)
+    start_date = models.CharField(max_length=255)
+    funding = models.CharField(max_length=255)
+    inquiry_reason = models.CharField(max_length=255)
     is_read_data_protection_policy = models.BooleanField()
     is_notification_opt_in = models.BooleanField()
