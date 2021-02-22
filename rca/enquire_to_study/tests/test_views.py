80eff008736e3c5e66a4792650f3c0a2540309e5
Brian Xu <brian.xu@torchbox.com>
2021-02-22 14:44:58 +0800

    use HTTPStatus package

--- rca/enquire_to_study/tests/test_views.py
+++ rca/enquire_to_study/tests/test_views.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
 
@@ -7,7 +9,7 @@ from rca.enquire_to_study.views import EnquireToStudyFormView
 class EnquireToStudyFormViewTest(TestCase):
     def test_path_existed(self):
         response = self.client.get(reverse("enquire_to_study:enquire_to_study_form"))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_environment_set_in_context(self):
         request = RequestFactory().get(reverse("enquire_to_study:enquire_to_study_form"))
@@ -21,4 +23,4 @@ class EnquireToStudyFormViewTest(TestCase):
 class EnquireToStudyFormThanksViewTest(TestCase):
     def test_path_existed(self):
         response = self.client.get(reverse("enquire_to_study:enquire_to_study_thanks"))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
