7ed756426e97e964c5673cb838e1939bef813bc2
Tom Readings <tom.readings@torchbox.com>
2021-02-24 16:43:13 +0000

    Add recommended filesize to document upload.

--- rca/documents/models.py
+++ rca/documents/models.py
@@ -1,6 +1,16 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 from wagtail.documents.models import AbstractDocument
 from wagtail.documents.models import Document as WagtailDocument
 
 
 class CustomDocument(AbstractDocument):
     admin_form_fields = WagtailDocument.admin_form_fields
+
+    # Provide help text for file field.
+    # Suggested filesize (unvalidated) avoids timeout during upload.
+    file = models.FileField(
+        upload_to='documents',
+        verbose_name=_('file'),
+        help_text=_('Maximum filesize: 10MB.'),
+    )
