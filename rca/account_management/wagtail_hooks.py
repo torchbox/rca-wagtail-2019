2eae625647b2e84436e9977914ef99cc1045a1d1
Kevin Howbrook <kbhowbrook@gmail.com>
2021-04-21 10:34:39 +0100

    Adds email notification and password reset link

--- rca/account_management/wagtail_hooks.py
+++ rca/account_management/wagtail_hooks.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.shortcuts import reverse
+from wagtail.admin.menu import MenuItem
 from wagtail.core import hooks
 
 from .views import CreateStudentFormView
@@ -13,3 +15,13 @@ def register_admin_urls():
             name="student_account_create",
         ),
     ]
+
+
+@hooks.register("register_admin_menu_item")
+def register_student_menu_item():
+    return MenuItem(
+        "Student accounts",
+        reverse("student_account_create"),
+        classnames="icon icon-user",
+        order=10000,
+    )
