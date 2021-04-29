12dd17ed2d04c10288211946d5b2be395b021c5c
Kevin Howbrook <kbhowbrook@gmail.com>
2021-04-29 11:49:16 +0100

    Rebase and fixup migrations

--- rca/programmes/migrations/0061_programmepage_mailchimp_group_name.py
+++ rca/programmes/migrations/0061_programmepage_mailchimp_group_name.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("programmes", "0059_add_qs_code"),
+        ("programmes", "0060_merge_20210429_1122"),
     ]
 
     operations = [
