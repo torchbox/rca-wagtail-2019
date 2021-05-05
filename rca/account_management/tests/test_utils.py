8b90558bd6ee5a5a007e4196a37a87de83bfd04a
Kevin Howbrook <kbhowbrook@gmail.com>
2021-05-05 09:53:53 +0100

    Improve tests
    Ensure the reset link returned matches what we expect it to

--- rca/account_management/tests/test_utils.py
+++ rca/account_management/tests/test_utils.py
@@ -8,4 +8,7 @@ class TestAccountManagementUtils(TestCase):
     def test_set_password_reset_link(self):
         user = UserFactory()
         reset_link = get_set_password_url(user)
-        self.assertTrue(reset_link)
+        self.assertRegex(
+            reset_link,
+            r"^/admin/password_reset/confirm/([0-9A-Za-z_\-]+)/([0-9A-Za-z]{1,6}-[0-9A-Za-z]{8,32})",
+        )
