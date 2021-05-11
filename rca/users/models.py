798225e678ea844c7c00530993d48166ab3a5f98
Kevin Howbrook <kbhowbrook@gmail.com>
2021-05-11 11:54:19 +0100

    Hide the admin search option for student users.
    This may break in 2.14 due to changes being made to the left hand admin
    area, so I've added a test.

--- rca/users/models.py
+++ rca/users/models.py
@@ -2,6 +2,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    def is_student(self):
+        return self.groups.filter(name="Students").exists()
+
     class Meta:
         ordering = [
             "username",
