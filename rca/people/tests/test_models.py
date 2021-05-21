9a778d42fa2e65e8c3ae013901133bdd873a2816
Kevin Howbrook <kbhowbrook@gmail.com>
2021-05-21 10:00:30 +0100

    wip tests

--- rca/people/tests/test_models.py
+++ rca/people/tests/test_models.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 from wagtail.tests.utils import WagtailPageTests
+from wagtail_factories import CollectionFactory
 
 from rca.home.models import HomePage
 from rca.people.factories import StudentIndexPageFactory, StudentPageFactory
 from rca.people.models import StudentIndexPage
+from rca.users.models import User
 
 
 class TestStudentPageFactory(TestCase):
@@ -28,3 +30,57 @@ class TestStudentIndexPage(WagtailPageTests):
         )
         # A second person listing page should not be creatable
         self.assertFalse(StudentIndexPage.can_create_at(self.home_page))
+
+
+class TestStudentPage(WagtailPageTests):
+    def setUp(self):
+        self.home_page = HomePage.objects.first()
+        self.user = self.login()
+        self.home_page.add_child(
+            instance=StudentIndexPage(
+                title="Students", slug="students", introduction="Students"
+            )
+        )
+        self.student = User.objects.create(
+            username="danascully",
+            first_name="dana",
+            last_name="Scully",
+            email="ds@fbi.com",
+            password="1234",
+        )
+
+        self.collection = CollectionFactory(name="Student: dana scully")
+
+    # def test_validation_of_user_field(self):
+    #     # on saving a student page with an image collection and student user,
+    #     # we expect to see a group with permissions added
+    #     self.student_index = StudentIndexPage.objects.first()
+    #     self.student_index.add_child(
+    #         instance=StudentPageFactory(
+    #             title="Dana Scully",
+    #             slug="a-student",
+    #             first_name="dana",
+    #             last_name="student",
+    #             student_user_account=self.student,
+    #         )
+    #     )
+    #     self.assertTrue(False)
+
+    # def test_group_and_rules_creation_on_save(self):
+    #     # on saving a student page with an image collection and student user,
+    #     # we expect to see a group with permissions added
+    #     # self.student.save()
+    #     # print(self.student)
+    #     self.student_index = StudentIndexPage.objects.first()
+    #     self.student_index.add_child(
+    #         instance=StudentPage(
+    #             title="Dana Scully",
+    #             slug="a-student",
+    #             first_name="dana",
+    #             last_name="student",
+    #             student_user_account=self.student,
+    #             student_user_image_collection = self.collection
+    #         )
+    #     )
+    #     print(StudentPage.objects.filter(title="Dana Scully"))
+    #     self.assertTrue(False)
