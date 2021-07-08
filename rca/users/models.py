949f8bbe0c9c8e9c111cb5a9978e6b9cc3c846f7
Tom Readings <tom.readings@torchbox.com>
2021-07-08 14:51:32 +0100

    Add groups to admin user listing (#669)

--- rca/users/models.py
+++ rca/users/models.py
@@ -1,9 +1,27 @@
 from django.contrib.auth.models import AbstractUser
+from django.utils.safestring import mark_safe
 
 
 class User(AbstractUser):
     def is_student(self):
         return self.groups.filter(name="Students").exists()
+
+    @property
+    def student_group_name(self):
+        """
+        The name of the single student group for this student
+        """
+        if self.is_student():
+            return f"Student: {self.username}"
+
+    def group_links(self):
+        groups = self.groups.all()
+        if self.is_student():
+            groups = self.groups.exclude(name=self.student_group_name)
+        return [
+            mark_safe(f'<a href="/admin/groups/{ g.pk }/users/">{ g.name }</a>')
+            for g in groups
+        ]
 
     class Meta:
         ordering = [
