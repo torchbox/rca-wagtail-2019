fcf3ec1816f57ed6909dad5dba86a343a092716f
Rich <rich.brennan@torchbox.com>
2021-07-08 11:24:04 +0100

    [873] finish event model, update template

---
+++ rca/events/tests.py
@@ -0,0 +1,18 @@
+from wagtail.images.tests.utils import get_test_image_file
+from wagtail.tests.utils import WagtailPageTests
+
+from rca.home.models import HomePage
+from rca.images.models import CustomImage
+from rca.standardpages.models import IndexPage, InformationPage
+
+from .models import EventDetailPage, EventIndexPage
+
+
+class EventDetailPageTests(WagtailPageTests):
+    def test_can_create_under_event_index_page(self):
+        self.assertCanCreateAt(EventIndexPage, EventDetailPage)
+
+    def test_cant_create_under_other_pages(self):
+        self.assertCanNotCreateAt(IndexPage, EventDetailPage)
+        self.assertCanNotCreateAt(InformationPage, EventDetailPage)
+        self.assertCanNotCreateAt(HomePage, EventDetailPage)
