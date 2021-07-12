9d66ddd744451647850874c792738394b38b3a82
Rich <rich.brennan@torchbox.com>
2021-07-12 14:04:33 +0100

    [880] remove trailing comma

--- rca/events/models.py
+++ rca/events/models.py
@@ -42,7 +42,7 @@ class EventDetailPage(BasePage):
     content_panels = BasePage.content_panels + [
         ImageChooserPanel("hero_image"),
         MultiFieldPanel(
-            [FieldPanel("start_date"), FieldPanel("end_date"), ], heading="Event Dates",
+            [FieldPanel("start_date"), FieldPanel("end_date")], heading="Event Dates",
         ),
         FieldPanel("introduction"),
         StreamFieldPanel("body"),
