2cb8e3769a174eeb8d5345e85ccb333460f127f7
Nick Moreton <nick.moreton@torchbox.com>
2021-07-15 12:48:14 +0100

    add tap landing page

--- rca/landingpages/models.py
+++ rca/landingpages/models.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 from modelcluster.fields import ParentalKey
 from wagtail.admin.edit_handlers import (
@@ -558,3 +559,48 @@ class EnterpriseLandingPage(LandingPage):
 
     class Meta:
         verbose_name = "Landing Page - Enterprise"
+
+
+class TapLandingPage(LandingPage):
+    template = "patterns/pages/landingpage/landing_page--tap.html"
+    tap_carousel = models.TextField(blank=True, verbose_name="Iframe Code")
+
+    class Meta:
+        verbose_name = "Landing Page - TAP"
+
+    content_panels = BasePage.content_panels + [
+        MultiFieldPanel([ImageChooserPanel("hero_image")], heading=_("Hero"),),
+        MultiFieldPanel(
+            [FieldPanel("introduction"), PageChooserPanel("about_page")],
+            heading=_("Introduction"),
+        ),
+        MultiFieldPanel([FieldPanel("tap_carousel")], heading="TAP Carousel"),
+        MultiFieldPanel(
+            [
+                FieldPanel("highlights_title"),
+                InlinePanel("related_pages_highlights", label=_("Page"), max_num=8),
+                PageChooserPanel("highlights_page_link"),
+                FieldPanel("highlights_page_link_title"),
+            ],
+            heading=_("Featured projects"),
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel("related_pages_title"),
+                FieldPanel("related_pages_text"),
+                InlinePanel("related_pages_grid", max_num=8, label=_("Related Pages")),
+            ],
+            heading=_("Related pages grid"),
+        ),
+        InlinePanel("featured_image", label=_("Featured content"), max_num=1),
+        FieldPanel("legacy_news_and_event_tags"),
+        MultiFieldPanel(
+            [FieldPanel("page_list_title"), StreamFieldPanel("page_list")],
+            heading=_("Related page list"),
+        ),
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context["tap_carousel"] = mark_safe(self.tap_carousel)
+        return context
