0ef6c4f1c4848faa8a5ec55117e36438a5a49a2f
Kevin Howbrook <kbhowbrook@gmail.com>
2021-07-22 17:50:37 +0100

    Rebase and correct migrations

--- rca/landingpages/migrations/0026_add_tap_landing_page.py
+++ rca/landingpages/migrations/0026_add_tap_landing_page.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("landingpages", "0024_remove_hero_colour_options"),
+        ("landingpages", "0025_add_contact_fields"),
     ]
 
     operations = [
