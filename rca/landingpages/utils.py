724154d56680cda7c61dc92524771514da712bed
Kevin Howbrook <kbhowbrook@gmail.com>
2021-07-27 14:01:32 +0100

    Add a better way to get consistent listing images

--- rca/landingpages/utils.py
+++ rca/landingpages/utils.py
@@ -1,12 +1,16 @@
+from rca.utils.models import get_listing_image
+
+
 def news_teaser_formatter(item, image=None):
     item_as_dict = {}
 
     editorial_type = item.editorial_types.first()
     item_as_dict["type"] = editorial_type.type if editorial_type else ""
 
-    if image and item.hero_image:
-        item_as_dict["image"] = item.hero_image.get_rendition("fill-392x284").url
-        item_as_dict["image_alt"] = item.hero_image.alt
+    image = get_listing_image(item)
+    if image:
+        item_as_dict["image"] = image.get_rendition("fill-392x284").url
+        item_as_dict["image_alt"] = image.alt
     item_as_dict["formatted_date"] = item.published_at
     item_as_dict["title"] = item.title
     item_as_dict["link"] = item.url
@@ -16,10 +20,10 @@ def news_teaser_formatter(item, image=None):
 
 def event_teaser_formatter(item, image=None):
     item_as_dict = {"type": item.event_type or ""}
-
-    if image and item.hero_image:
-        item_as_dict["image"] = item.hero_image.get_rendition("fill-392x284").url
-        item_as_dict["image_alt"] = item.hero_image.alt
+    image = get_listing_image(item)
+    if image:
+        item_as_dict["image"] = image.get_rendition("fill-392x284").url
+        item_as_dict["image_alt"] = image.alt
     item_as_dict["formatted_date"] = (
         str(item.start_date.strftime("%-d %b %Y")) + ", Location (TODO)"
     )
@@ -34,9 +38,10 @@ def editorial_teaser_formatter(item):
     editorial_type = item.editorial_types.first()
     item_as_dict["meta"] = editorial_type.type if editorial_type else ""
 
-    if item.hero_image:
-        item_as_dict["image"] = item.hero_image
-        item_as_dict["image_alt"] = item.hero_image.alt
+    image = get_listing_image(item)
+    if image:
+        item_as_dict["image"] = image
+        item_as_dict["image_alt"] = image.alt
     item_as_dict["formatted_date"] = item.published_at
     item_as_dict["title"] = item.title
     item_as_dict["link"] = item.url
