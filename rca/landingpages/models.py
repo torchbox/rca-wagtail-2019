126ca83b08642c9f8c10cf892d85960320ca0f98
Kevin Howbrook <kevin.howbrook@torchbox.com>
2021-08-17 16:36:11 +0100

    Exposes fields for the alumni related pages grid

--- rca/landingpages/models.py
+++ rca/landingpages/models.py
@@ -40,6 +40,7 @@ from rca.utils.models import (
     LegacyNewsAndEventsMixin,
     LinkFields,
     RelatedPage,
+    get_listing_image,
 )
 
 
@@ -351,9 +352,7 @@ class LandingPage(ContactFieldsMixin, LegacyNewsAndEventsMixin, BasePage):
                     {
                         "title": page.title,
                         "link": page.url,
-                        "image": page.listing_image
-                        if hasattr(page, "hero_image") and page.hero_image
-                        else page.listing_image,
+                        "image": get_listing_image(page),
                         "description": page.introduction
                         if hasattr(page, "introduction")
                         else page.listing_summary,
@@ -882,6 +881,13 @@ class AlumniLandingPage(LandingPage):
             heading="Video",
         ),
         FieldPanel("body"),
+        MultiFieldPanel(
+            [
+                FieldPanel("related_pages_text"),
+                InlinePanel("related_pages_grid", max_num=8, label=_("Related Pages")),
+            ],
+            heading=_("Related pages grid"),
+        ),
         # latest
         FieldPanel("latest_intro"),
         InlinePanel(
@@ -957,4 +963,5 @@ class AlumniLandingPage(LandingPage):
         context["get_involved"] = self._format_slideshow_pages(
             self.alumni_slideshow_page.all()
         )
+        context["page_teasers"] = self.get_related_pages(self.related_pages_grid)
         return context
