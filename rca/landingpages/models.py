ed2d13667fa1e6574749d1216e1b699b95d20073
Kevin Howbrook <kevin.howbrook@torchbox.com>
2021-08-19 16:45:21 +0100

    Needs uppercase L

--- rca/landingpages/models.py
+++ rca/landingpages/models.py
@@ -982,7 +982,7 @@ class AlumniLandingPage(LandingPage):
         in-page navigation """
         return [
             {"title": "Alumni benefits", "link": "alumni-benefits"},
-            {"title": "latest", "link": "latest"},
+            {"title": "Latest", "link": "latest"},
             {"title": "Get involved", "link": "get-involved"},
             {"title": "Stay connected", "link": "stay-connected"},
         ]
