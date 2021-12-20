15d99a7a5bdf3bcbe5d05df243dfbd147200cb81
Nick Lee <nick@nicklee.co.uk>
2021-12-20 12:42:16 +0000

    Disabling max-nesting-depth
    please us responsibly ;)

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
     // See https://github.com/torchbox/stylelint-config-torchbox for rules.
     extends: 'stylelint-config-torchbox',
+    rules: {
+        'max-nesting-depth': null,
+    },
 };
