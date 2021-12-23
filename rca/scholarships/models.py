2ac81120cf0f7183fae421b177a6dd2e01354dc7
Kevin <kevin.howbrook@torchbox.com>
2021-12-23 10:46:09 +0000

    Adds ScholarshipListingPage base and template

--- rca/scholarships/models.py
+++ rca/scholarships/models.py
@@ -1,1 +1,14 @@
-# Create your models here.
+from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
+
+from rca.utils.models import BasePage, ContactFieldsMixin
+
+
+class ScholarshipsListingPage(ContactFieldsMixin, BasePage):
+    template = "patterns/pages/scholarships/scholarships_listing_page.html"
+    max_count = 1
+    introduction = models.CharField(max_length=500, blank=True)
+    content_panels = BasePage.content_panels + [
+        FieldPanel("introduction"),
+        MultiFieldPanel([*ContactFieldsMixin.panels], heading="Contact information"),
+    ]
--- rca/scholarships/tests.py
+++ rca/scholarships/tests.py
@@ -1,1 +1,24 @@
-# Create your tests here.
+from django.test import TestCase
+from wagtail.tests.utils import WagtailPageTests
+
+from rca.home.models import HomePage
+from rca.scholarships.factories import ScholarshipsListingPageFactory
+from rca.scholarships.models import ScholarshipsListingPage
+
+
+class TestScholarshipsListingPageFactory(TestCase):
+    def test_factories(self):
+        ScholarshipsListingPageFactory()
+
+
+class TestScholarshipsListingPageRules(WagtailPageTests):
+    def setUp(self):
+        self.home_page = HomePage.objects.first()
+
+    def test_can_create(self):
+        self.assertCanCreateAt(HomePage, ScholarshipsListingPage)
+
+    def test_singlet(self):
+        self.home_page.add_child(instance=ScholarshipsListingPage(title="Scholarships"))
+        # A second ScholarshipsListingPage should not be creatable
+        self.assertFalse(ScholarshipsListingPage.can_create_at(self.home_page))
