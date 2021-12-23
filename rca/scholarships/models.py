c8f2e0d5002cb92abff8397c25515a406acba2d0
Kevin <2128707+kevinhowbrook@users.noreply.github.com>
2021-12-23 15:37:05 +0000

    Adds body fields and anchor navigation for scholarships (#822)

--- rca/scholarships/models.py
+++ rca/scholarships/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
+from django.utils.text import slugify
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
 
+from rca.scholarships.blocks import ScholarshipsListingPageBlock
 from rca.utils.models import BasePage, ContactFieldsMixin, SluggedTaxonomy
 
 
@@ -29,7 +32,32 @@ class ScholarshipsListingPage(ContactFieldsMixin, BasePage):
     template = "patterns/pages/scholarships/scholarships_listing_page.html"
     max_count = 1
     introduction = models.CharField(max_length=500, blank=True)
+    body = StreamField(ScholarshipsListingPageBlock())
+    lower_body = StreamField(ScholarshipsListingPageBlock())
+
     content_panels = BasePage.content_panels + [
         FieldPanel("introduction"),
+        StreamFieldPanel("body"),
+        StreamFieldPanel("lower_body"),
         MultiFieldPanel([*ContactFieldsMixin.panels], heading="Contact information"),
     ]
+
+    def anchor_nav(self):
+        """ Build list of data to be used as in-page navigation """
+        items = []
+        blocks = []
+
+        for block in self.body:
+            blocks.append(block)
+        for block in self.lower_body:
+            blocks.append(block)
+
+        for i, block in enumerate(blocks):
+            if block.block_type == "anchor_heading":
+                items.append({"title": block.value, "link": f"#{slugify(block.value)}"})
+        return items
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context["anchor_nav"] = self.anchor_nav()
+        return context
