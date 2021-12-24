cbda2d73b6347bf56f670733c6ce4adb0b5b5dad
Kevin <2128707+kevinhowbrook@users.noreply.github.com>
2021-12-24 11:57:01 +0000

    2365 listing page form settings (#824)
    * Adds body fields and anchor navigation for scholarships
    
    * Adds form fields for publishers to curate areas on the scholarship form

--- rca/scholarships/models.py
+++ rca/scholarships/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.utils.text import slugify
-from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, StreamFieldPanel
-from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    MultiFieldPanel,
+    ObjectList,
+    StreamFieldPanel,
+    TabbedInterface,
+)
+from wagtail.core.fields import RichTextField, StreamField
 
 from rca.scholarships.blocks import ScholarshipsListingPageBlock
+from rca.utils.blocks import CallToActionBlock
 from rca.utils.models import BasePage, ContactFieldsMixin, SluggedTaxonomy
 
 
@@ -33,14 +40,45 @@ class ScholarshipsListingPage(ContactFieldsMixin, BasePage):
     max_count = 1
     introduction = models.CharField(max_length=500, blank=True)
     body = StreamField(ScholarshipsListingPageBlock())
+    characteristics_disclaimer = models.CharField(
+        max_length=250,
+        blank=True,
+        help_text="A small disclaimer shown just above the scholarships listing.",
+    )
     lower_body = StreamField(ScholarshipsListingPageBlock())
+
+    key_details = RichTextField(features=["h3", "bold", "italic", "link"], blank=True)
+    form_introduction = models.CharField(max_length=500, blank=True)
+    cta_block = StreamField(
+        [("call_to_action", CallToActionBlock(label="text promo"))],
+        blank=True,
+        verbose_name="Call to action",
+    )
 
     content_panels = BasePage.content_panels + [
         FieldPanel("introduction"),
         StreamFieldPanel("body"),
+        MultiFieldPanel(
+            [FieldPanel("characteristics_disclaimer")], heading="Scholarship listing"
+        ),
         StreamFieldPanel("lower_body"),
         MultiFieldPanel([*ContactFieldsMixin.panels], heading="Contact information"),
     ]
+
+    form_settings_pannels = [
+        FieldPanel("key_details"),
+        FieldPanel("form_introduction"),
+        StreamFieldPanel("cta_block"),
+    ]
+
+    edit_handler = TabbedInterface(
+        [
+            ObjectList(content_panels, heading="Content"),
+            ObjectList(BasePage.promote_panels, heading="Promote"),
+            ObjectList(BasePage.settings_panels, heading="Settings"),
+            ObjectList(form_settings_pannels, heading="Enquiry form settings"),
+        ]
+    )
 
     def anchor_nav(self):
         """ Build list of data to be used as in-page navigation """
