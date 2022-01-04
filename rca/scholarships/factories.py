7712e2d5cb1137f1624037287fb570b18fcfac2c
Kevin <2128707+kevinhowbrook@users.noreply.github.com>
2022-01-04 12:43:11 +0000

    Adds scholarship form submission object and delete view (#825)
    * Adds scholarship form submission object and delete view
    
    * dump migration for rebase
    
    * re-init migration
    
    * Isort
    
    * Add scholarship to the form submission

--- rca/scholarships/factories.py
+++ rca/scholarships/factories.py
@@ -7,6 +7,7 @@ from faker import Factory as FakerFactory
 from rca.programmes.factories import ProgrammePageFactory
 from rca.scholarships.models import (
     Scholarship,
+    ScholarshipEnquiryFormSubmission,
     ScholarshipFeeStatus,
     ScholarshipFunding,
     ScholarshipLocation,
@@ -86,3 +87,16 @@ class ScholarshipsListingPageFactory(wagtail_factories.PageFactory):
 
     title = factory.Faker("text", max_nb_chars=25)
     introduction = factory.Faker("text", max_nb_chars=500)
+
+
+class ScholarshipEnquiryFormSubmissionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ScholarshipEnquiryFormSubmission
+
+    first_name = factory.Sequence(lambda n: "test-firstname-%d" % n)
+    last_name = factory.Sequence(lambda n: "test-lastname-%d" % n)
+    email = factory.Sequence(lambda n: "test-email-%d@example.org" % n)
+    rca_id_number = faker.random_number()
+    is_read_data_protection_policy = True
+    is_notification_opt_in = True
+    programme = factory.SubFactory(ProgrammePageFactory)
--- rca/scholarships/views.py
+++ rca/scholarships/views.py
@@ -1,1 +1,47 @@
-# Create your views here.
+from datetime import timedelta
+
+from django.core.exceptions import PermissionDenied
+from django.db.models import Q
+from django.shortcuts import redirect, reverse
+from django.template.response import TemplateResponse
+from django.utils import timezone
+from wagtail.admin import messages
+
+from rca.scholarships.models import ScholarshipEnquiryFormSubmission
+
+
+def scholarships_delete(request):
+    if not request.user.has_perm(
+        "scholarship_enquiry_form_submission:can_delete_scholarship_enquiry_form_submission"
+    ):
+        raise PermissionDenied()
+
+    from .wagtail_hooks import ScholarshipEnquiryFormSubmissionAdmin
+
+    url_helper = ScholarshipEnquiryFormSubmissionAdmin().url_helper
+    index_url = url_helper.get_action_url("index")
+
+    all_submissions = ScholarshipEnquiryFormSubmission.objects.all()
+
+    time_threshold = timezone.now() - timedelta(days=7)
+    instances = all_submissions.filter(Q(submission_date__lte=time_threshold))
+    count_delete_submissions = len(instances)
+
+    if request.method == "POST":
+        instances.delete()
+
+        message_content = f"deleted {count_delete_submissions} submissions"
+        messages.success(request, message_content)
+
+        return redirect(index_url)
+
+    return TemplateResponse(
+        request,
+        "scholarships/confirm_delete.html",
+        {
+            "count_all_submissions": len(all_submissions),
+            "count_delete_submissions": count_delete_submissions,
+            "index_url": url_helper.get_action_url("index"),
+            "submit_url": (reverse("scholarships_delete")),
+        },
+    )
