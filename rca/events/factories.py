5ab15acbebc359d28c8646aa258fa53f6fa2eb41
Kevin <2128707+kevinhowbrook@users.noreply.github.com>
2022-05-24 15:14:41 +0100

    #2241 BUG - Error 500 when importing items to intranet (#875)
    * When data for these fields is None, imports on the intranet fail
    
    * Fix tests no we have new required fields

--- rca/events/factories.py
+++ rca/events/factories.py
@@ -26,24 +26,6 @@ class EventTypeFactory(factory.django.DjangoModelFactory):
     title = factory.Faker("text", max_nb_chars=25)
 
 
-class EventDetailPageFactory(wagtail_factories.PageFactory):
-    class Meta:
-        model = EventDetailPage
-
-    title = factory.Faker("text", max_nb_chars=25)
-    introduction = factory.Faker("text", max_nb_chars=150)
-    start_date = factory.Faker("date")
-    end_date = factory.Faker("date")
-    event_type = factory.SubFactory(EventTypeFactory)
-
-
-class EventAvailabilityFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = EventAvailability
-
-    title = factory.Faker("text", max_nb_chars=25)
-
-
 class EventLocationFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = EventLocation
@@ -54,3 +36,23 @@ class EventLocationFactory(factory.django.DjangoModelFactory):
 class EventEligibilityFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = EventEligibility
+
+
+class EventDetailPageFactory(wagtail_factories.PageFactory):
+    class Meta:
+        model = EventDetailPage
+
+    title = factory.Faker("text", max_nb_chars=25)
+    introduction = factory.Faker("text", max_nb_chars=150)
+    start_date = factory.Faker("date")
+    end_date = factory.Faker("date")
+    event_type = factory.SubFactory(EventTypeFactory)
+    eligibility = factory.SubFactory(EventEligibilityFactory)
+    location = factory.SubFactory(EventLocationFactory)
+
+
+class EventAvailabilityFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = EventAvailability
+
+    title = factory.Faker("text", max_nb_chars=25)
