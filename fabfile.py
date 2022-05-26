ec55b7698fc09904ae055f549fd1c20d1627e323
Brian Xu <brian.xu@torchbox.com>
2022-05-26 19:12:05 +0800

    update fab script

--- fabfile.py
+++ fabfile.py
@@ -214,7 +214,7 @@ def pull_production_images(c):
 @task
 def pull_production_data(c):
     """Pull database from production Heroku Postgres"""
-    pull_database_from_heroku(c, PRODUCTION_APP_INSTANCE)
+    pull_database_from_heroku(c, PRODUCTION_APP_INSTANCE, anonymise=True)
 
 
 # @task
@@ -416,7 +416,7 @@ def pull_media_from_s3_heroku(c, app_instance):
     )
 
 
-def pull_database_from_heroku(c, app_instance):
+def pull_database_from_heroku(c, app_instance, anonymise=False):
     datestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
     local(
@@ -433,6 +433,9 @@ def pull_database_from_heroku(c, app_instance):
             datestamp=datestamp,
         ),
     )
+
+    if anonymise:
+        dexec("./manage.py run_birdbath --skip-checks")
 
 
 def open_heroku_shell(c, app_instance, shell_command="bash"):
