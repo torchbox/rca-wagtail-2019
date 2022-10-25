cc3f87492d02139091e848f7146878686ce159a2
nick.moreton <nick.moreton@torchbox.com>
2022-10-25 16:18:27 +0100

    Hide the search bar for student users

--- rca/people/wagtail_hooks.py
+++ rca/people/wagtail_hooks.py
@@ -8,3 +8,19 @@ def editor_js():
     return mark_safe(
         '<script src="%s"></script>' % static("people/admin/js/update_person_title.js")
     )
+
+
+@hooks.register("insert_global_admin_js")
+def global_admin_js():
+    # hide the form once the page has loaded
+    return mark_safe(
+        '<script src="%s"></script>' % static("people/admin/js/student_side_bar.js")
+    )
+
+
+@hooks.register("insert_global_admin_css")
+def global_admin_css():
+    # while the page is loading, hide the form to avoid a flash of the form
+    return mark_safe(
+        '<script src="%s"></script>' % static("people/admin/css/student_side_bar.css")
+    )
