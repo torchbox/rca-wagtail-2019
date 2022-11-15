82df77281d986a978bf8fab489a7e54f8937293e
Kevin <kbhowbrook@gmail.com>
2022-11-15 17:24:18 +0000

    Ensure editors can see search

--- rca/account_management/tests/test_admin.py
+++ rca/account_management/tests/test_admin.py
@@ -9,8 +9,12 @@ class TestStudentAdmin(TestCase):
     def setUp(self):
         self.user = UserFactory(is_superuser=True)
         student_group = Group.objects.get(name="Students")
+        editor_group = Group.objects.get(name="Editors")
         self.student_user = UserFactory()
         self.student_user.groups.add(student_group)
+        self.editor = UserFactory()
+        self.editor.groups.add(editor_group)
+
         admin_permission = Permission.objects.get(codename="access_admin")
         student_group.permissions.add(admin_permission)
         self.student_user.save()
@@ -20,6 +24,15 @@ class TestStudentAdmin(TestCase):
         response = self.client.get("/admin/")
         self.assertEqual(response.status_code, 200)
 
+        soup = bs4(response.content, "html.parser")
+        sidebar = soup.find("aside")
+        self.assertIsNotNone(sidebar)
+        self.assertFalse("data-student-sidebar" in sidebar.attrs)
+
+    def test_search_is_visible_for_editors(self):
+        self.client.force_login(self.editor)
+        response = self.client.get("/admin/")
+        self.assertEqual(response.status_code, 200)
         soup = bs4(response.content, "html.parser")
         sidebar = soup.find("aside")
         self.assertIsNotNone(sidebar)
