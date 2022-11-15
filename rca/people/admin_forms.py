b3bbcdb9d1bb378d47476a454a357f7c76a4cd9f
nick.moreton <nick.moreton@torchbox.com>
2022-11-15 18:05:21 +0000

    save

--- rca/people/admin_forms.py
+++ rca/people/admin_forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from wagtail.admin.forms import WagtailAdminPageForm
 
 
@@ -6,23 +7,74 @@ class StudentPageAdminForm(WagtailAdminPageForm):
 
     def __init__(self, *args, **kwargs):
         super(StudentPageAdminForm, self).__init__(*args, **kwargs)
+
+        user = kwargs.get("for_user")
+        user_level = None
+        """
+        'is_active', 'is_anonymous', 'is_authenticated', 'is_staff', 'is_student', 'is_superuser'
+        """
+        if user.is_superuser:
+            user_level = "superuser"
+        elif user.is_student():
+            user_level = "student"
+        else:
+            user_level = "locked"
+        # print("superuser", user.is_superuser)
+        # print("admin", user.is_student())
+        # print(user.__dir__())
+        # permission = None
+        # if user.is_student():
+        #     permission = "student"
+        # elif user.is_admin():
+        #     permission = "admin"
+        # self.is_student = user.is_student()
+        # print("is_student", self.is_student)
+
         # Don't allow changing the user account or image collection once set as they are tied to permissions
         if (
             self.instance.student_user_image_collection
             and self.instance.student_user_account
         ):
-            readonly_fields = [
+            disabled_fields = [
                 "student_user_image_collection",
                 "student_user_account",
-                "title",
             ]
-            for f in readonly_fields:
+            for f in disabled_fields:
                 # Wrapped in a try except as when a student uses this form, these fields are hidden
                 # See rca.people.models.StudentPage.basic_content_panels and rca.people.utils.PerUserPageMixin
                 try:
                     self.fields[f].widget.attrs["disabled"] = True
                 except KeyError:
                     pass
+
+        if self.instance.title and self.instance.slug and not user_level == "student":
+            readonly_fields = [
+                "title",
+                "slug",
+            ]
+
+            for f in readonly_fields:
+                self.fields[f].widget.attrs["readonly"] = True
+
+        elif self.instance.title and self.instance.slug and user_level == "student":
+            hidden_fields = [
+                "title",
+                "slug",
+            ]
+
+            for tab in self.instance.edit_handler.children:
+                for panel in tab.children:
+                    if panel.__class__.__name__ == "FieldPanel":
+                        if panel.field_name in hidden_fields:
+                            panel.permission = "hidden"
+            #         if hasattr(panel, "field_name") and panel.field_name in hidden_fields:
+            #             panel.is_shown = False
+
+            # if panel.heading in hidden_fields:
+            #     panel.heading = None
+
+            for f in hidden_fields:
+                self.fields[f].widget = forms.HiddenInput()
 
     def clean(self):
         cleaned_data = super().clean()
