45a6098ca9059c94e1d6d38c297bfb7923c21448
Patrick Gan <patrick.gan@torchbox.com>
2022-12-07 14:11:35 +0800

    Add image with caption block to editorial page block

--- rca/editorial/blocks.py
+++ rca/editorial/blocks.py
@@ -5,7 +5,10 @@ from rca.utils import blocks as utils_blocks
 
 
 class EditorialPageBlock(blocks.StreamBlock):
+    # There might be editorial pages that uses currently use `image` so we'll want
+    # to keep this in here.
     image = utils_blocks.ImageChooserBlock()
+    image_with_caption = utils_blocks.ImageBlock()
     heading = blocks.CharBlock(
         form_classname="full title",
         icon="title",
