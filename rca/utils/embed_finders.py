f6f7f5680acb5cca5aed2687d032c8f2dd42db5e
Patrick Gan <patrick.gan@torchbox.com>
2024-05-31 17:52:57 +0800

    Add wix embed finder

--- rca/utils/embed_finders.py
+++ rca/utils/embed_finders.py
@@ -1,5 +1,8 @@
+import re
+import requests
 from bs4 import BeautifulSoup
 from wagtail.embeds.finders.oembed import OEmbedFinder
+from wagtail.embeds.finders.base import EmbedFinder
 
 
 class CustomOEmbedFinder(OEmbedFinder):
@@ -20,3 +23,30 @@ class CustomOEmbedFinder(OEmbedFinder):
             embed["html"] = str(soup)
 
         return embed
+
+
+class WixEmbedFinder(EmbedFinder):
+    """Embed finder support for Wix. Wix does not have oEmbed support."""
+
+    def accept(self, url):
+        print("hi?")
+        return re.match(r'^https?://(?:www\.)?embed.wix\.com/.+$', url)
+
+    def find_embed(self, url, max_width=None):
+        
+        # Attempt to fetch the page content
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Attempt to scrape the title
+        title_tag = soup.find('title')
+        title = title_tag.string if title_tag else 'No title found'
+
+        print(title);
+
+        return {
+            'title': title,
+            'provider_name': 'Wix',
+            'type': 'video',  # Adjust according to the type of content (photo, video, rich, etc.)
+            'html': '<iframe src="{url}" frameborder="0" allowfullscreen title={title}></iframe>'.format(url=url, title=title),
+        }
