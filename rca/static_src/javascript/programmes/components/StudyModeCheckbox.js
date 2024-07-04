c0e7253d4e283c9df02c32d949064462bd7ce2ae
katdom13 <katdom13@gmail.com>
2024-07-04 12:04:26 +0800

    Update UI, remove handleToggle

--- rca/static_src/javascript/programmes/components/StudyModeCheckbox.js
+++ rca/static_src/javascript/programmes/components/StudyModeCheckbox.js
@@ -1,37 +1,45 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { getCourseLengthURL, pushState } from '../programmes.routes';
 
 const ModeCheckbox = ({ ariaLabel, activeLength }) => {
-    const [isPartTime, setIsPartTime] = useState(activeLength);
+    const [isFullTime, setIsFullTime] = useState(false);
+    const [isPartTime, setIsPartTime] = useState(false);
 
-    const handleToggle = () => {
-        setIsPartTime(!isPartTime);
+    useEffect(() => {
+        console.log('Full-time');
+    }, [isFullTime]);
 
-        // Update the URL with the new Part-time value
-        const url = getCourseLengthURL(isPartTime ? '' : 'true');
-        pushState(url);
-    };
+    useEffect(() => {
+        console.log('Part-time');
+    }, [isPartTime]);
 
     return (
         <div className="mode-checkbox" aria-label={ariaLabel}>
+            <input
+                type="checkbox"
+                className="mode-checkbox__checkbox"
+                id="is-full-time"
+                checked={isFullTime}
+                onChange={(e) => setIsFullTime(!isFullTime)}
+            />
             <label
                 htmlFor="is-full-time"
                 className={`mode-checkbox__label${
-                    !isPartTime ? ' mode-checkbox__label--selected' : ''
+                    isFullTime ? ' mode-checkbox__label--selected' : ''
                 }`}
                 data-label="full-time"
             >
-                <input
-                    type="checkbox"
-                    className="mode-checkbox__checkbox"
-                    id="is-full-time"
-                    checked={!isPartTime}
-                    onChange={handleToggle}
-                />
                 Full-time
             </label>
+            <input
+                type="checkbox"
+                className="mode-checkbox__checkbox"
+                id="is-part-time"
+                checked={isPartTime}
+                onChange={(e) => setIsPartTime(!isPartTime)}
+            />
             <label
                 htmlFor="is-part-time"
                 className={`mode-checkbox__label${
@@ -39,13 +47,6 @@ const ModeCheckbox = ({ ariaLabel, activeLength }) => {
                 }`}
                 data-label="part-time"
             >
-                <input
-                    type="checkbox"
-                    className="mode-checkbox__checkbox"
-                    id="is-part-time"
-                    checked={isPartTime}
-                    onChange={handleToggle}
-                />
                 Part-time
             </label>
         </div>
