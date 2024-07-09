3ddca2cb2b816d257de531998c7f083f62a3d404
katdom13 <katdom13@gmail.com>
2024-07-09 15:43:06 +0800

    Add checkbox validation error message

--- rca/static_src/javascript/programmes/components/StudyModeCheckbox.js
+++ rca/static_src/javascript/programmes/components/StudyModeCheckbox.js
@@ -6,8 +6,12 @@ import { getCourseLengthURL, pushState } from '../programmes.routes';
 const ModeCheckbox = ({ ariaLabel, isFullTime, isPartTime }) => {
     const [_isFullTime, setIsFullTime] = useState(isFullTime);
     const [_isPartTime, setIsPartTime] = useState(isPartTime);
+    const [showError, setShowError] = useState(false);
 
     useEffect(() => {
+        // Hide error when at least one checkbox is checked
+        setShowError(!_isFullTime && !_isPartTime);
+
         // Update the URL with the new Part-time value
         const url = getCourseLengthURL(
             String(_isFullTime),
@@ -17,40 +21,57 @@ const ModeCheckbox = ({ ariaLabel, isFullTime, isPartTime }) => {
     }, [_isFullTime, _isPartTime]);
 
     return (
-        <div className="mode-checkbox" aria-label={ariaLabel}>
-            <label
-                htmlFor="is-full-time"
-                className={`mode-checkbox__label${
-                    _isFullTime ? ' mode-checkbox__label--selected' : ''
-                }`}
-                data-label="full-time"
-            >
-                <input
-                    type="checkbox"
-                    className="mode-checkbox__checkbox"
-                    id="is-full-time"
-                    checked={_isFullTime}
-                    onChange={() => setIsFullTime(!_isFullTime)}
-                />
-                Full-time
-            </label>
-            <label
-                htmlFor="is-part-time"
-                className={`mode-checkbox__label${
-                    _isPartTime ? ' mode-checkbox__label--selected' : ''
-                }`}
-                data-label="part-time"
-            >
-                <input
-                    type="checkbox"
-                    className="mode-checkbox__checkbox"
-                    id="is-part-time"
-                    checked={_isPartTime}
-                    onChange={() => setIsPartTime(!_isPartTime)}
-                />
-                Part-time
-            </label>
-        </div>
+        <>
+            <div className="mode-checkbox" aria-label={ariaLabel}>
+                {showError && (
+                    <div className="mode-checkbox__error">
+                        You must select at least one mode of study.
+                    </div>
+                )}
+                <div className="mode-checkbox__formset">
+                    <label
+                        htmlFor="is-full-time"
+                        className={`mode-checkbox__label${
+                            _isFullTime ? ' mode-checkbox__label--selected' : ''
+                        }`}
+                        data-label="full-time"
+                    >
+                        <input
+                            type="checkbox"
+                            className={`mode-checkbox__checkbox${
+                                showError
+                                    ? ' mode-checkbox__checkbox--error'
+                                    : ''
+                            }`}
+                            id="is-full-time"
+                            checked={_isFullTime}
+                            onChange={() => setIsFullTime(!_isFullTime)}
+                        />
+                        Full-time
+                    </label>
+                    <label
+                        htmlFor="is-part-time"
+                        className={`mode-checkbox__label${
+                            _isPartTime ? ' mode-checkbox__label--selected' : ''
+                        }`}
+                        data-label="part-time"
+                    >
+                        <input
+                            type="checkbox"
+                            className={`mode-checkbox__checkbox${
+                                showError
+                                    ? ' mode-checkbox__checkbox--error'
+                                    : ''
+                            }`}
+                            id="is-part-time"
+                            checked={_isPartTime}
+                            onChange={() => setIsPartTime(!_isPartTime)}
+                        />
+                        Part-time
+                    </label>
+                </div>
+            </div>
+        </>
     );
 };
 
