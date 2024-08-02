267c5c63bcda2dc508ec982fe6444e6551039994
Andy Babic <andy.babic@torchbox.com>
2024-08-02 16:44:35 +0100

    Add canonical tag to site head (#1026)

--- rca/utils/context_processors.py
+++ rca/utils/context_processors.py
@@ -2,4 +2,9 @@ from django.conf import settings
 
 
 def global_vars(request):
-    return {"GOOGLE_TAG_MANAGER_ID": getattr(settings, "GOOGLE_TAG_MANAGER_ID", None)}
+    return {
+        "GOOGLE_TAG_MANAGER_ID": getattr(settings, "GOOGLE_TAG_MANAGER_ID", None),
+        "CANONICAL_HOST": (
+            ("https://" if request.is_secure() else "http://") + request.get_host()
+        ),
+    }
