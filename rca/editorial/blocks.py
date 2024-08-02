d9c9cc7e0cb7677957bbb6c35ccf52dc816c5f0a
Andy Babic <andy.babic@torchbox.com>
2024-08-02 15:35:37 +0100

    Fix: Broken ImageChooserBlock and EmbedBlock imports (#1023)
    * Fix broken import in editorial.blocks
    
    * Fix invalid imports in events.blocks
    
    * Fix invalid import in scholarships.blocks
    
    (cherry picked from commit 4ec7a5d47bcbdaa711fa645ed1dc6b8fd0b9498e)

--- rca/editorial/blocks.py
+++ rca/editorial/blocks.py
@@ -1,5 +1,6 @@
 from wagtail import blocks
 from wagtail.embeds.blocks import EmbedBlock
+from wagtail.images.blocks import ImageChooserBlock
 
 from rca.utils import blocks as utils_blocks
 
@@ -7,7 +8,7 @@ from rca.utils import blocks as utils_blocks
 class EditorialPageBlock(blocks.StreamBlock):
     # There might be editorial pages that uses currently use `image` so we'll want
     # to keep this in here.
-    image = utils_blocks.ImageChooserBlock()
+    image = ImageChooserBlock()
     image_with_caption = utils_blocks.ImageBlock()
     heading = blocks.CharBlock(
         form_classname="full title",
