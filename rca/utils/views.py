42826e2b62c283c662c65bd887a8b45c409b708b
Andy Babic <andy.babic@torchbox.com>
2024-08-08 15:52:55 +0100

    Add mixin for class-based-views to use

--- rca/utils/views.py
+++ rca/utils/views.py
@@ -1,6 +1,18 @@
 from django.views import defaults
 
 
+class MetaTitleMixin:
+    meta_title = ""
+
+    def get_meta_title(self):
+        return self.meta_title
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context["meta_title"] = self.get_meta_title()
+        return context
+
+
 def page_not_found(request, exception, template_name="patterns/pages/errors/404.html"):
     return defaults.page_not_found(request, exception, template_name)
 
