f0c6317ff33c812d93f88baa66d2b0bd8adfe8f3
Andy Babic <andy.babic@torchbox.com>
2024-08-08 09:24:06 +0100

    Remove now-unused CANONICAL_HOST from context

--- rca/utils/context_processors.py
+++ rca/utils/context_processors.py
@@ -4,7 +4,4 @@ from django.conf import settings
 def global_vars(request):
     return {
         "GOOGLE_TAG_MANAGER_ID": getattr(settings, "GOOGLE_TAG_MANAGER_ID", None),
-        "CANONICAL_HOST": (
-            ("https://" if request.is_secure() else "http://") + request.get_host()
-        ),
     }
