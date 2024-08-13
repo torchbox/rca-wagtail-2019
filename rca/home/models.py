f1e287db338559bbf6ccc2c8e41fa0ac80f53cfa
Andy Babic <andy.babic@torchbox.com>
2024-08-13 14:59:08 +0100

    Remove redundant 'use_api_for_alumni_stories' booleanfield

--- rca/home/models.py
+++ rca/home/models.py
@@ -155,7 +155,6 @@ class HomePage(TapMixin, BasePage):
     strapline = models.CharField(max_length=125)
     strapline_cta_url = models.URLField(blank=True)
     strapline_cta_text = models.CharField(max_length=125, blank=True)
-    use_api_for_alumni_stories = models.BooleanField(default=True)
     use_api_for_news_and_events = models.BooleanField(default=True)
     news_and_events_link_text = models.TextField(
         max_length=120,
@@ -216,7 +215,6 @@ class HomePage(TapMixin, BasePage):
                             </ul>"""
                         )
                     ),
-                    FieldPanel("use_api_for_alumni_stories"),
                     FieldPanel("use_api_for_news_and_events"),
                 ],
                 heading="News, Events and Alumni Stories Content Listings",
